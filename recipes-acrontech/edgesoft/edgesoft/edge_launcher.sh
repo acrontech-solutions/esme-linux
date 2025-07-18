701fe877e24fcea352a8526894f64e3df2b70c1a
Csaba Hegedűs <csaba.hegedus@acrontech.eu>
2025-07-18 13:27:47 +0200

    Update edge_launcher.sh

--- recipes-acrontech/edgesoft/edgesoft/edge_launcher.sh
+++ recipes-acrontech/edgesoft/edgesoft/edge_launcher.sh
@@ -8,20 +8,20 @@ while [ $should_run -eq 0 ]
 do
 	echo "Starting EdgeSoft."
 	dmesg -n 1
-	if [ ! -e /home/root/passive.fifo ]; then
-		mkfifo /home/root/passive.fifo
-        mkfifo /home/root/miso.fifo
-		mkfifo /home/root/mosi.fifo
+	if [ ! -e /root/passive.fifo ]; then
+		mkfifo /root/passive.fifo
+        mkfifo /root/miso.fifo
+		mkfifo /root/mosi.fifo
     fi
 
 	start_timestamp=$(date +%s)
-	/home/root/current/edgesoft
+	/root/current/edgesoft
 	return_code=$?
 	echo "EdgeSoft exited."
 	run_time=$(( $(date +%s) - start_timestamp ))
 	#update if there's anything to update
-	cp -a /home/root/new/. /home/root/current/
-	rm -rf /home/root/new/*
+	cp -a /root/new/. /root/current/
+	rm -rf /root/new/*
 	#then either exit edge_launcher, reboot the system, shutdown, or relaunch edgesoft
 	if [ $return_code -eq 8 ]
 	then
@@ -40,11 +40,11 @@ do
 		if [ $quick_crash_count -eq 10 ]
 		then
 			echo "Crashed too many times, restoring backup version."
-			cp -a /home/root/backup/. /home/root/current/
+			cp -a /root/backup/. /root/current/
 		elif [ $quick_crash_count -eq 20 ]
 		then
 			echo "Crashed too many times, restoring factory version."
-			cp -a /home/root/factory/. /home/root/current/
+			cp -a /root/factory/. /root/current/
 		fi
 	elif [ $run_time -ge 3600 ]
 	then
